111ceb2bc4fee438de5a84805747f81d9ddf303b
raghdaali <raghdaali991@gmail.com>
2025-07-24 01:40:13 +0300

    feat: add initial light theme setup and remove debug banner from app

--- lib/config/routers/app_router.dart
+++ lib/config/routers/app_router.dart
@@ -1,6 +1,4 @@
-import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-import 'package:movie_stars/core/constants/route_names.dart';
 
 class AppRouter {
   static final GoRouter router = GoRouter(
