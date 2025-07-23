6d26dd28185887d38f6b837bd24938082b4bc9fa
raghdaali <raghdaali991@gmail.com>
2025-07-24 01:33:33 +0300

    feat: add initial app router setup using go_router

--- lib/config/routers/app_router.dart
+++ lib/config/routers/app_router.dart
@@ -1,1 +1,15 @@
-// This file will handle all app routes configuration.
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:movie_stars/core/constants/route_names.dart';
+
+class AppRouter {
+  static final GoRouter router = GoRouter(
+    routes: [
+      GoRoute(
+        path: '/',
+        // name: RouteNames.homePage,
+        // builder: (context, state) => const PopularPeoplePage(),
+      ),
+    ],
+  );
+}
