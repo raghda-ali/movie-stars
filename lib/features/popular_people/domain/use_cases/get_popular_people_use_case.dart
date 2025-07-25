e9ca6aa0e1842f34633ac932a86c1fc697384f17
raghdaali <raghdaali991@gmail.com>
2025-07-26 01:24:51 +0300

    chore: remove unused imports

--- lib/features/popular_people/domain/use_cases/get_popular_people_use_case.dart
+++ lib/features/popular_people/domain/use_cases/get_popular_people_use_case.dart
@@ -1,6 +1,5 @@
 import 'package:dartz/dartz.dart';
 import 'package:movie_stars/core/exceptions/dio_exceptions.dart';
-import 'package:movie_stars/features/popular_people/domain/entities/person_entity.dart';
 import 'package:movie_stars/features/popular_people/domain/entities/person_response_entity.dart';
 import 'package:movie_stars/features/popular_people/domain/repositories/people_repository.dart';
 
--- lib/service_locator.dart
+++ lib/service_locator.dart
@@ -1,4 +1,3 @@
-import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
 import 'package:internet_connection_checker/internet_connection_checker.dart';
