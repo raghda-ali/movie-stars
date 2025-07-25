0e601c525aadcb2b1be3e5c3d1640c010b01ade0
raghdaali <raghdaali991@gmail.com>
2025-07-25 16:18:05 +0300

    refactor: improve error handling structure in person details UI

--- lib/features/popular_people/presentation/pages/person_details_page.dart
+++ lib/features/popular_people/presentation/pages/person_details_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:movie_stars/core/extensions/show_snackbar.dart';
 import 'package:movie_stars/core/shared_widgets/custom_cached_network_image.dart';
 import 'package:movie_stars/features/popular_people/presentation/bloc/popular_people_bloc.dart';
 import 'package:movie_stars/features/popular_people/presentation/widgets/person_images_widget.dart';
@@ -26,7 +27,22 @@ class _PersonDetailsPageState extends State<PersonDetailsPage> {
   Widget build(BuildContext context) {
     final popularPeopleBloc = BlocProvider.of<PopularPeopleBloc>(context);
     return Scaffold(
-      body: BlocBuilder<PopularPeopleBloc, PopularPeopleState>(
+      body: BlocConsumer<PopularPeopleBloc, PopularPeopleState>(
+        listener: (context, state) {
+          if (state.personBasicInfoStatus == RequestStatus.error) {
+            context.showSnackBar(
+              context: context,
+              message:
+                  state.personBasicInfoError ?? "Failed to load person info",
+            );
+          } else if (state.personImagesStatus == RequestStatus.error) {
+            context.showSnackBar(
+              context: context,
+              message:
+                  state.personImagesError ?? 'Failed to load person images',
+            );
+          }
+        },
         builder: (context, state) {
           final basicInfo = popularPeopleBloc.personBasicInfo;
           return CustomScrollView(
@@ -53,72 +69,81 @@ class _PersonDetailsPageState extends State<PersonDetailsPage> {
                   padding: const EdgeInsets.all(15.0),
                   sliver: SliverList(
                     delegate: SliverChildListDelegate([
-                      Center(
-                        child: Container(
-                          width: 150,
-                          height: 150,
-                          decoration: const BoxDecoration(
-                            shape: BoxShape.circle,
+                      if (state.personBasicInfoStatus == RequestStatus.error)
+                        Center(
+                          child: Text(
+                            "Failed to load person details, try again later",
                           ),
-                          clipBehavior: Clip.hardEdge,
-                          child: CustomCachedNetworkImage(
-                            imageUrl:
-                                basicInfo?.profilePath ??
-                                'assets/images/user.png',
-                            fit: BoxFit.cover,
+                        )
+                      else ...[
+                        Center(
+                          child: Container(
+                            width: 150,
+                            height: 150,
+                            decoration: const BoxDecoration(
+                              shape: BoxShape.circle,
+                            ),
+                            clipBehavior: Clip.hardEdge,
+                            child: CustomCachedNetworkImage(
+                              imageUrl:
+                                  basicInfo?.profilePath ??
+                                  'assets/images/user.png',
+                              fit: BoxFit.cover,
+                            ),
                           ),
                         ),
-                      ),
-                      const SizedBox(height: 20),
-                      Text(
-                        basicInfo?.name ?? '',
-                        style: const TextStyle(
-                          fontWeight: FontWeight.w700,
-                          fontSize: 20,
-                        ),
-                      ),
-                      Text(
-                        basicInfo?.knownForDepartment ?? '',
-                        style: const TextStyle(fontSize: 17),
-                      ),
-                      const SizedBox(height: 5),
-                      if (basicInfo?.birthday?.isNotEmpty == true)
-                        Text('Birthday: ${basicInfo!.birthday}'),
-                      const SizedBox(height: 5),
-                      if (basicInfo?.placeOfBirth?.isNotEmpty == true)
-                        Text('Place of birth: ${basicInfo!.placeOfBirth}'),
-                      const SizedBox(height: 15),
-                      if ((basicInfo?.alsoKnownAs?.isNotEmpty ?? false)) ...[
-                        const Text(
-                          'Also known as:',
-                          style: TextStyle(
-                            fontSize: 17,
+                        const SizedBox(height: 20),
+                        Text(
+                          basicInfo?.name ?? '',
+                          style: const TextStyle(
                             fontWeight: FontWeight.w700,
+                            fontSize: 20,
                           ),
                         ),
-                        ...basicInfo!.alsoKnownAs!.map(
-                          (name) => Text('- $name'),
-                        ),
-                        const SizedBox(height: 15),
-                      ],
-
-                      if ((basicInfo?.biography?.isNotEmpty ?? false)) ...[
-                        const Text(
-                          'Description',
-                          style: TextStyle(
-                            fontSize: 17,
-                            fontWeight: FontWeight.w700,
-                          ),
+                        Text(
+                          basicInfo?.knownForDepartment ?? '',
+                          style: const TextStyle(fontSize: 17),
                         ),
                         const SizedBox(height: 5),
-                        Text(basicInfo!.biography!),
+                        if (basicInfo?.birthday?.isNotEmpty == true)
+                          Text('Birthday: ${basicInfo!.birthday}'),
+                        const SizedBox(height: 5),
+                        if (basicInfo?.placeOfBirth?.isNotEmpty == true)
+                          Text('Place of birth: ${basicInfo!.placeOfBirth}'),
                         const SizedBox(height: 15),
+                        if ((basicInfo?.alsoKnownAs?.isNotEmpty ?? false)) ...[
+                          const Text(
+                            'Also known as:',
+                            style: TextStyle(
+                              fontSize: 17,
+                              fontWeight: FontWeight.w700,
+                            ),
+                          ),
+                          ...basicInfo!.alsoKnownAs!.map(
+                            (name) => Text('- $name'),
+                          ),
+                          const SizedBox(height: 15),
+                        ],
+
+                        if ((basicInfo?.biography?.isNotEmpty ?? false)) ...[
+                          const Text(
+                            'Description',
+                            style: TextStyle(
+                              fontSize: 17,
+                              fontWeight: FontWeight.w700,
+                            ),
+                          ),
+                          const SizedBox(height: 5),
+                          Text(basicInfo!.biography!),
+                          const SizedBox(height: 15),
+                        ],
                       ],
-                      if ((popularPeopleBloc
-                              .personImagesResponse
-                              ?.profiles
-                              .isNotEmpty ??
-                          false)) ...[
+                      if (state.personBasicInfoStatus != RequestStatus.error &&
+                          (popularPeopleBloc
+                                  .personImagesResponse
+                                  ?.profiles
+                                  .isNotEmpty ??
+                              false)) ...[
                         const Text(
                           'Images',
                           style: TextStyle(
